659f1769db46fa26b18e840343b51c67304396f1
ranadudukabak <77778044+ranadudukabak@users.noreply.github.com>
2021-07-15 15:19:55 +0300

    Update Gozluklu_Sirin.java

--- src/prolab_2_proje_1/Gozluklu_Sirin.java
+++ src/prolab_2_proje_1/Gozluklu_Sirin.java
@@ -1,19 +1,10 @@
-///*
-// * To change this license header, choose License Headers in Project Properties.
-// * To change this template file, choose Tools | Templates
-// * and open the template in the editor.
-// */
 package prolab_2_proje_1;
-
-/**
- *
- * @author ranadudukabak
- */
 
 /*
    Her defasinda 2 birim ilerler. Azman’a dokunursa oyuncu 5
    puan kaybeder. Gargamel’e dokunursa oyuncu 15 puan kaybeder.
 */
+
 public class Gozluklu_Sirin extends Karakter {
     //Constructorlar
     public Gozluklu_Sirin() {
@@ -37,23 +28,20 @@ public class Gozluklu_Sirin extends Karakter {
 //        super(oyuncuID, oyuncuAd, oyuncuTur, skor);
 //    }
 
-    
 //    //Override Edilen Methodlar
 //    public void Ilerle() {//Oyuncu 2 birim ilerler.
 //        
 //    }
-//    
-////   
+    
 //    public void OyuncuAzmanaDokundu() {//Oyuncu 5 puan kaybeder.
 //        
 //    }
-//    
-//  
+    
 //    public void OyuncuGargameleDokundu() {//Oyuncu 15 puan kaybeder.
 //        
 //    }
 //
-//   
+    
 //    public void PuaniGoster() {
 //        
 //    }
