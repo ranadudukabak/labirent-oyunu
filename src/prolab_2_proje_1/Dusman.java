df8649bc9e54fd749fd9171aa41beba00370f0eb
ranadudukabak <77778044+ranadudukabak@users.noreply.github.com>
2021-07-15 15:16:50 +0300

    Update Dusman.java

--- src/prolab_2_proje_1/Dusman.java
+++ src/prolab_2_proje_1/Dusman.java
@@ -1,14 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package prolab_2_proje_1;
-
-/**
- *
- * @author ranadudukabak
- */
 
 /*
    Bu sinifta karakter sinifi kalitim olarak verilecektir. Constructor, Get ve Set metotları yer
