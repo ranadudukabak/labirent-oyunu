3d4d3623bc4d12c4335e093de23404af25929dfe
ranadudukabak <77778044+ranadudukabak@users.noreply.github.com>
2021-07-15 15:22:52 +0300

    Update Mantar.java

--- src/prolab_2_proje_1/Mantar.java
+++ src/prolab_2_proje_1/Mantar.java
@@ -1,14 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package prolab_2_proje_1;
-
-/**
- *
- * @author ranadudukabak
- */
 
 /*
 Random olarak (maksimum 20 saniye olacak şekilde) herhangi bir yerde mantar
